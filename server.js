6de4c7d45e6a34a93b58fd1a9cebf8e8c8ce75c4
DanielMoran98 <33605732+DanielMoran98@users.noreply.github.com>
2018-09-14 03:55:01 +0100

    -Added a landing page

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res)
 
     var weatherData = {weather: weather};
     console.log(weatherData);
-    res.render('weather', weatherData);
+    res.render('index', weatherData);
 
   });
 });
@@ -51,9 +51,10 @@ app.post('/city', urlencodedParser, function(req, res)
       }
     weatherArray.push(loopObject);
   } //console.log(weatherArray);
-    var weather = {
-      city: city, //Sets the city taken from the search bar
-      temperature: Math.round(weatherJson.list[0].main.temp - 273.15), //Sets the values taken from the API query
+
+    var weather = { //Gets accurate current weather
+      city: city,
+      temperature: Math.round(weatherJson.list[0].main.temp - 273.15),
       description: weatherJson.list[0].weather[0].main,
       icon: weatherJson.list[0].weather[0].icon
     };
